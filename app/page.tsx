09518f1555b119ea899d86e2aa302ffaca81c493
kayne-lee <kayne.lee2@outlook.com>
2024-04-28 17:37:10 -0400

    finish personal interests

--- app/page.tsx
+++ app/page.tsx
@@ -18,12 +18,14 @@ import { usePastExperiences } from "@/components/home/PastExperiences";
 import { useTechStack } from "@/components/home/TechStack";
 import { useExtra } from "@/components/home/Extra"
 import { useProjects } from "@/components/home/Projects";
+import { useInterests } from "@/components/home/Interests";
 
 export default function Home() {
   const { setShowPastExperiences, PastExperiences } = usePastExperiences();
   const { setShowTechStack, TechStack } = useTechStack();
   const { setShowExtra, Extra } = useExtra();
   const { setShowProjects, Projects } = useProjects();
+  const { setShowInterests, Interests } = useInterests();
   
   return (
     <>
@@ -179,7 +181,8 @@ export default function Home() {
         <Projects />
         {/* Feature 6 */}
         <div
-          className="flex flex-col relative items-center justify-center col-span-1 h-[300px] overflow-hidden rounded-xl border border-gray-200 bg-white shadow-md"
+        onClick={() => setShowInterests(true)}
+          className="flex cursor-pointer flex-col relative items-center justify-center col-span-1 h-[300px] overflow-hidden rounded-xl border border-gray-200 bg-white shadow-md"
         >
           <div className="h-[60%] flex flex-col justify-between align-center">
             <div className="mx-auto max-w-md text-2xl font-display text-center ">
@@ -195,6 +198,7 @@ export default function Home() {
             </div>
           </div>
         </div>
+        <Interests />
       </div>
     </>
   );
---
+++ components/home/Interests.tsx
@@ -0,0 +1,88 @@
+import Modal from "@/components/shared/modal";
+import {
+  useState,
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useMemo,
+} from "react";
+import HOCKEY from "public/Hockey.png"
+import FOOTBALL from "public/Football.png"
+import SKATE from "public/Skate.png"
+import SHOE from "public/Shoe.png"
+import WORKOUT from "public/Workout.png"
+import Image from "next/image";
+
+const Interests = ({
+  showInterests,
+  setShowInterests,
+}: {
+  showInterests: boolean;
+  setShowInterests: Dispatch<SetStateAction<boolean>>;
+}) => {
+  return (
+    <Modal showModal={showInterests} setShowModal={setShowInterests}>
+      <div className="w-full overflow-hidden md:max-w-4xl md:rounded-2xl md:border md:border-gray-100 md:shadow-xl">
+        <div className="flex flex-col items-center justify-center space-y-3 bg-white px-4 py-6 pt-8 text-center md:px-16">
+            <h3 className="font-display text-2xl font-bold">Personal Interests</h3>
+            <div className="bg-[#4A4A4A] w-[100%] h-0.5"></div>
+            <div className="flex flex-row flex-wrap gap-5 justify-center">
+                    {INTERESTS.map((interests) => (
+                        <div className="flex flex-col gap-5 border-2 rounded-xl border-gray-200 p-5 items-center">
+                            {interests.name}
+                            <Image 
+                            className="rounded-lg"
+                                  src={interests.image}
+                                  alt="QTMA"
+                                  width={100}
+                                  />
+                        </div>
+                    ))}
+            </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export function useInterests() {
+  const [showInterests, setShowInterests] = useState(false);
+
+  const DemoModalCallback = useCallback(() => {
+    return (
+      <Interests
+        showInterests={showInterests}
+        setShowInterests={setShowInterests}
+      />
+    );
+  }, [showInterests, setShowInterests]);
+
+  return useMemo(
+    () => ({ setShowInterests, Interests: DemoModalCallback }),
+    [setShowInterests, DemoModalCallback],
+  );
+}
+
+export const INTERESTS = [
+    {
+        name: "Hockey",
+        image: HOCKEY,
+    },
+    {
+        name: "Football",
+        image: FOOTBALL,
+    },
+    {
+        name: "Skateboarding",
+        image: SKATE,
+    },
+    {
+        name: "Working Out",
+        image: WORKOUT,
+    },
+    {
+        name: "Shoe Collection",
+        image: SHOE,
+    },
+];
+    
