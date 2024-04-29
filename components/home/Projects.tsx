d2fac25042dc5be0b78cbd5f4f0e5bc775cd8020
kayne-lee <kayne.lee2@outlook.com>
2024-04-28 20:55:40 -0400

    fix bugs

--- components/home/Projects.tsx
+++ components/home/Projects.tsx
@@ -30,7 +30,7 @@ const Projects = ({
                 <div className="flex flex-row gap-5 flex-wrap justify-center">
                     {PROJECTS.map((work) => (
                         <button
-                          key={project.name}
+                          key={work.name}
                             className="text-xl"
                             onClick={() => {
                             setProject(work);
@@ -47,7 +47,7 @@ const Projects = ({
                   <h2 className="font-bold text-lg">{project.name}</h2>
                   <div className="flex flex-row justify-center gap-5">
                     {project.technologies.map((tech) => (
-                      <div key={project.name} className="border-2 rounded-xl border-gray-200 px-5">
+                      <div key={tech} className="border-2 rounded-xl border-gray-200 px-5">
                         <p>{tech}</p>
                       </div>
                       ))}
