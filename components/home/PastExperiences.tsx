3336e370d63d45fa7b63afb30115119c5cfa2cc0
kayne-lee <kayne.lee2@outlook.com>
2024-10-20 16:27:46 -0400

    [edit] update past experiences

--- components/home/PastExperiences.tsx
+++ components/home/PastExperiences.tsx
@@ -25,28 +25,43 @@ const PastExperiences = ({
           <h3 className="font-display text-2xl font-bold">Past Experiences</h3>
           <div className="bg-[#4A4A4A] w-[100%] h-0.5"></div>
             <div className="flex flex-col gap-3">
-              <div className="flex flex-row justify-between">
+            <div className="flex flex-row justify-between">
                 <div className="flex flex-col  gap-1">
                   <div className="flex flex-row justify-between">
-                    <h2 className="text-left text-xl font-bold">Teaching Assistant <span className="font-normal text-[#4A4A4A]"> - Queen&apos;s Univesity</span></h2>
-                    <span className="font-normal">Winter 2024</span>
+                    <h2 className="text-left text-xl font-bold">Software Engineer Intern<span className="font-normal text-[#4A4A4A]"> - STraffic America</span></h2>
+                    <span className="font-normal">May - August 2024</span>
                   </div>
-                  <p className=" text-left text-sm">Delivered comprehensive support to over 200 students specializing in computer programming concepts ensuring proficiency. Addressed
-                    individual queries with precision, fostering a strong foundation of key principles and delivering feedback on assignments for improvement.
-                    Demonstrated leadership, communication, and problem-solving skills through active engagement with students and collaborative teamwork.
-                    Leveraged cutting-edge tools such as CLion and Gradescope, streamlining processes and facilitating a dynamic exchange of information.</p>
+                  <p className=" text-left text-sm">
+                  Developed computer vision models for fare collection, processing 300,000+ images with 97% accuracy using YOLOv10X, resulting in $10
+Million in additional revenues from fare evasion detection. Created and managed API endpoints ensuring secure data transmission via SSL/TLS and conducted real-time data processing for client-server communications. Architected and deployed an internal calendar system using Spring Boot and React, integrating RESTful APIs for efficient data handling
+and employing Spring Security for robust user protection.
+                  </p>
                 </div>
               </div>
               <div className="flex flex-row justify-between">
                 <div className="flex flex-col  gap-1">
                   <div className="flex flex-row justify-between">
-                    <h2 className="text-left text-xl font-bold">Software Engineer Intern <span className="font-normal text-[#4A4A4A]"> - Debacle inc.</span></h2>
-                    <span className="font-normal">Winter 2024</span>
+                    <h2 className="text-left text-xl font-bold">Teaching Assistant <span className="font-normal text-[#4A4A4A]"> - Queen&apos;s Univesity</span></h2>
+                    <span className="font-normal">January - April 2024</span>
                   </div>
-                  <p className=" text-sm text-left">Engineered a highly effective Shopify plug-in app, leveraging data from over 250 Shopify stores worldwide. Employing JSON, Python, and
-                    advanced machine-learning techniques to generate keywords for user recommendation enhancing user engagement and sales conversion rates.
-                    Implemented secure authentication with Shopify AUTH and optimized query efficiency with GraphQL resulting in a 30% reduction in response time
-                    Addressed and debugged complex challenges including Admin Block issues resulting in a 40% increase in app stability and optimized performance.</p>
+                  <p className=" text-left text-sm">
+                  Guided 200+ students in mastering computer programming concepts, providing targeted feedback and resolving individual queries to ensure
+proficiency. Directed 5 team-based design projects, guiding students in applying engineering principle and managing project timelines, collaboration, and
+integration of technical, economic, and social factors.
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-row justify-between">
+                <div className="flex flex-col  gap-1">
+                  <div className="flex flex-row justify-between">
+                    <h2 className="text-left text-xl font-bold">Front End Developer<span className="font-normal text-[#4A4A4A]"> - Musical Moon</span></h2>
+                    <span className="font-normal">January - April 2024</span>
+                  </div>
+                  <p className=" text-sm text-left">
+                  Constructed the front-end for a social web platform using Next.js, serving over 1,000 active users, and reducing load times by 30% through
+server-side rendering. Implemented routing and authentication to deliver personalized and efficiently retrieve user-specific data. Integrated real-time data fetching using Next.js API routes for instant updates to user profiles, posts, and notifications, improving platform.
+Collaborated on UI/UX improvements, enhancing user engagement by designing intuitive interfaces and smooth navigation flows.
+                  </p>
                 </div>
               </div>
             </div>
